4f88606081a5a15500d279906493a8a8017181b6
Jorge Hernandez <jorgekevinhl@ufps.edu.co>
2022-04-29 22:22:31 -0500

    Noticia 1 y 6 finalizadas

--- js/noticia1.js
+++ js/noticia1.js
@@ -1,50 +1,33 @@
 const fecha = document.getElementById('fecha')
 date = new Date();
 year = date.getFullYear();
-month = date.getMonth()+1;
+month = date.getMonth() + 1;
 day = date.getDate();
-fecha.innerHTML = day + " de " + month +" de " + year; 
+fecha.innerHTML = day + " de " + month + " de " + year;
 
 const aplicacion = document.querySelector('.principal')
 
-const url = "https://carlosreneas.github.io/endpoints/noticias.json"
+const url = "https://carlosreneas.github.io/endpoints/noticia_1.json"
 
 fetch(url)
     .then(res => res.json())
     .then(data => {
-        data.forEach((usuario, i) => {
-            if (i === 0) {
-                const img = document.createElement('img')
-                const espacio_img = document.querySelector('.imagen')
-                img.src = usuario.img
-                img.setAttribute("style", "width:100%; height:100%;")
-                espacio_img.appendChild(img)
-            }
+        const h = document.createElement("h2")
+        const div = document.createElement("div")
+        const img = document.createElement("img")
+        img.src = data.img;
+        img.setAttribute("style", "width:100%; height:100%;")
+        div.setAttribute("style", "width: 80%; height: 60vh; border: 1px solid #000;")
+        h.setAttribute("style", "color: blue; font-weight: bold")
+        h.innerHTML = data.titulo + "-" + data.categoria + "-" + data.fecha
+        aplicacion.appendChild(h);
+        div.appendChild(img);
+        aplicacion.appendChild(div);
 
-            if (i != 3) {
-                const h = document.createElement('h2')
-                const p = document.createElement('p')
-                const a = document.createElement('a')
-                a.href = '../html/noticia1.html'
-                if(i === 2){
-                    a.href = '../html/noticia6.html'
-                }else{
-                    a.href = "#"
-                }
-                
-                
-                a.innerText = 'Ver más'
-                a.setAttribute("style", "color: #000;")
-                p.setAttribute("style", "border: 1px dotted #000")
-                h.setAttribute("style", "color: blue; font-weight: bold")
-                h.innerHTML = usuario.titulo + "-" + usuario.categoria + "-" + usuario.fecha
-                p.innerHTML = usuario.descripcion + ".  "
-                aplicacion.appendChild(h)
-                p.appendChild(a)
-                aplicacion.appendChild(p)
-
-            }
-        });
+        const p = document.createElement('p')
+        p.setAttribute("style", "border: 1px dotted #000")
+        p.innerHTML = data.descripcion + ".  " + data.detalle
+        aplicacion.appendChild(p)
     })
     .catch(err => console.log(err))
 
